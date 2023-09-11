0c892175ccca19499642bf131b66889039145052
Will Hodgkinson <willhodgkinson5@gmail.com>
2023-09-11 20:08:05 +0100

    test deploy 2

--- public/js/albums/get-albums.js
+++ public/js/albums/get-albums.js
@@ -1,6 +1,6 @@
 const getAlbums = async (id) => {
   console.log("Calling get albums")
-  const response = await fetch(`http://${window.location.host}/artists/${id}/albums`);
+  const response = await fetch(`${window.location.origin}/artists/${id}/albums`);
   const data = await response.json();
   console.log(data);
   return data;
--- public/js/albums/post-album.js
+++ public/js/albums/post-album.js
@@ -3,37 +3,31 @@ async function postAlbum(event, artist_id) {
 
   const name = document.getElementById('albumNameField').value;
   const year = document.getElementById('albumYearField').value;
-  const cover_image = document.getElementById('albumCoverField').files[0];
-
+  var cover_image;
+  if (document.body.contains(document.getElementById('albumCoverField')) && document.getElementById('albumCoverField').files.length > 0) {
+    cover_image = document.getElementById('albumCoverField').files[0];
+    console.log(cover_image);
+  } else {
+    console.log("No cover image");
+  }
+  
   const formData = new FormData();
   
   formData.append('name', name);
   formData.append('year', year);
 
-  let response;
-
-  if (cover_image) {
+  if(cover_image) {
     formData.append('cover_image', cover_image);
-    response = await fetch(`http://${window.location.host}/artists/${artist_id}/albums`, {
+  }
+  
+  const response = await fetch(`${window.location.origin}/artists/${artist_id}/albums`, {
       method: 'POST',
       body: formData
-    });
-  } else {
-    const jsonData = JSON.stringify(Object.fromEntries(formData));
-
-    response = await fetch(`http://${window.location.host}/artists/${artist_id}/albums`, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: jsonData
-    });
-  }
+  })
 
   if (!response.ok) {
     window.alert('Oops: Something went wrong :(');
   } else {
-    window.location.replace(`http://${window.location.host}/html/artist-profile.html?artistId=${artist_id}`);
+    window.location.replace(`${window.location.origin}/html/artist-profile.html?artistId=${artist_id}`);
   }
 }
